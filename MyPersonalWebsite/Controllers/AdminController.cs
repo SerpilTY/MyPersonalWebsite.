c8a5d9879e4ade4328617a409ceccf4182cf09a8
Serpil <serpilturanyuksel@gmail.com>
2023-10-11 00:26:11 +0300

    Update Functions and Controllers Added

--- MyPersonalWebsite/Controllers/AdminController.cs
+++ MyPersonalWebsite/Controllers/AdminController.cs
@@ -16,5 +16,22 @@ namespace MyPersonalWebsite.Controllers
             var value = c.HomePages.ToList();
             return View(value);
         }
+        public ActionResult HomePageDetails(int id)
+        {
+            var hpdetails = c.HomePages.Find(id);
+            return View("HomePageDetails", hpdetails);
+        }
+
+        public ActionResult UpdateHomePage(HomePage x) {
+            var hpdetails = c.HomePages.Find(x.Id);
+            hpdetails.UserName = x.UserName;
+            hpdetails.Title = x.Title;
+            hpdetails.Description = x.Description;
+            hpdetails.Contact = x.Contact;
+            hpdetails.Profile = x.Profile;
+            c.SaveChanges();
+            return RedirectToAction("Index"); 
+        
+        }
     }
 }
